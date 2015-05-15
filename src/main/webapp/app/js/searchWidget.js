f580923cb3c7b887493a6027a4d989ca3820d771
Ram Mittala <seetharamireddy540@gmail.com>
2015-05-14 22:56:47 -0700

    RES

--- src/main/webapp/app/js/searchWidget.js
+++ src/main/webapp/app/js/searchWidget.js
@@ -1,16 +1,12 @@
 var app = angular.module("searchWidget", []);
 
 app.config(function($httpProvider) {
-<<<<<<< HEAD
 
 	//Reset headers to avoid OPTIONS request (aka preflight)
   $httpProvider.defaults.headers.common = {};
   $httpProvider.defaults.headers.post = {};
   $httpProvider.defaults.headers.put = {};
   $httpProvider.defaults.headers.patch = {};
-
-=======
->>>>>>> origin/master
 	$httpProvider.defaults.transformRequest = function(data) {
 		 if (data === undefined) {
 			 return data;
@@ -21,7 +17,6 @@ app.config(function($httpProvider) {
 	
 });
 
-<<<<<<< HEAD
 app.controller("TripsController", ['$http','$window', '$filter', function($http, $window, $filter) {
 	
 		var testCtrl = this;
@@ -66,53 +61,5 @@ app.controller("TripsController", ['$http','$window', '$filter', function($http,
 		};
 	
 
-
-}]);
-
-app.controller("PostParametersController", ['$http','$window', function($http, $window) {
-	
-		var testCtrl = this;
-		testCtrl.responseTxt ='';
-		testCtrl.showResponse = false;
-	
-		testCtrl.sendEmail = function() {
-			var req = {
-					method : 'POST',
-			 		url :'http://localhost:8080/SinglePageApplication/hello',
-			 		headers: { 'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'},
-   					data : {email : this.emailAddress}
-			}
-			$http(req).success(function(data, status, headers, config) {
-					testCtrl.responseTxt = data.currentDateStr;
-					testCtrl.showResponse = true;
-
-			}).error(function(data, status, headers, config) {
-					testCtrl.responseTxt  = status;
-			});
-		};
-=======
-app.controller("TestPostParams", ['$http','$window', function($http, $window) {
-
-
-	this.sendEmail = function() {
-
-		 var req = {
-					 method : 'POST',
-			 		 url :'http://localhost:8080/SinglePageApplication/hello',
-			 		 headers: { 'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'},
-    				 data : {email : this.emailAddress}
-			 }
-			 $http(req).success(function(data, status, headers, config) {
-					this.res = status;
-					this.ram = status;
-
-			 }).error(function(data, status, headers, config) {
-					this.res = status;
-
-			 });
-
-
-	}
->>>>>>> origin/master
 
 }]);
